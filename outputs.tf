95e7f45856c02f28e6662a1cc40cb3964f7bcfa6
Panos Georgantas <panos@georgantas.info>
2025-10-03 12:31:01 +0300

    add outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,15 @@ output "fqdn" {
   value = resource.azurerm_postgresql_flexible_server.main.fqdn
 }
 
+output "name" {
+  value = resource.azurerm_postgresql_flexible_server.main.name
+}
+
+output "private_dns_zone_id" {
+  value = resource.azurerm_postgresql_flexible_server.main.private_dns_zone_id
+}
+
+output "id" {
+  value = resource.azurerm_postgresql_flexible_server.main.id
+}
+
